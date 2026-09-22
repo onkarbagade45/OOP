ae59b507986b83d8ddf9a088001887cad24d4bc7
𝙾𝚗𝚔𝚊𝚛 𝙱𝚊𝚐𝚊𝚍𝚎 <onkarbagade4508@gmail.com>
2026-09-22 18:43:20 +0530

    Update Assignment4.cpp

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -1,6 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class book()
-{
-  public:
-         int
+
+class Book {
+    string title;
+    string author;
+    int price;
+
+public:
+    Book() {
+        title = "ABC";
+        author = "DEF";
+        price = 100;
+    }
+
+    Book(string t, string a, int p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    void display() {
+        cout << "Book Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: Rs. " << price << endl;
+    }
+};
+
+int main() {
+    Book b1;
+
+    Book b2("OOP", "XYZ", 350);
+
+    cout << "Book 1:" << endl;
+    b1.display();
+
+    cout << "\nBook 2:" << endl;
+    b2.display();
+
+    return 0;
+}
